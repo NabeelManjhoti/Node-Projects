4070b265fbd44fe073db33ee84f258e126415386
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-03-24 20:51:38 +0500

    first project

---
+++ Project01-Simple-Calculator/index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt ([
+    {message: "Enter first number", type: "number", name: "firstNumber"},
+    {message: "Enter second number", type: "number", name: "secondNumber"},
+    {
+        message: "Select one of Operator",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Substrction", "Multiplication", "Devision"],
+    },
+]);
+
+if(answer.operator === "Addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if(answer.operator === "Substraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+} else if(answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber);
+} else if(answer.operator === "Devision"){
+    console.log(answer.firstNumber / answer.secondNumber);
+} else{
+    console.log("Please select one of valid operator to perform task");
+};
