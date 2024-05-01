b98180fc36467e24f3961e514db53eafcc57d9ee
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-05-01 13:06:09 +0500

    Assignment-Self-Check-Quiz

---
+++ Assignment-Project-Self-Check-Quiz/main.js
@@ -0,0 +1,12 @@
+// Self-Check Quiz
+var answer = "right";
+if (answer === "correct") {
+    console.log("your answer is correct");
+}
+else if (answer === "incorrect") {
+    console.log("your answer is incorrect");
+}
+else {
+    console.log("Error");
+}
+;
---
+++ Assignment-Project-Self-Check-Quiz/main.ts
@@ -0,0 +1,10 @@
+// Self-Check Quiz
+let answer:string = "correct";
+
+if (answer === "correct") {
+    console.log("your answer is correct");    
+} else if (answer === "incorrect") {
+    console.log("your answer is incorrect");    
+} else {
+    console.log("Error");
+};
